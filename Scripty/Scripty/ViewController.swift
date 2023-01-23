cf419f842621e0833778aa5253c008dbcf0af717
Michael Testut <michael.testut@yahoo.com>
2023-01-22 20:22:14 -0800

    started xcode project

---
+++ Scripty/Scripty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Scripty
+//
+//  Created by Michael Testut on 1/22/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
