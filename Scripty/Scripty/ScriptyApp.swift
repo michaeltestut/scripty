08bba7cc936016bd13bea84664f5a22dddb07829
Michael Testut <michael.testut@yahoo.com>
2023-02-01 16:26:57 -0800

    changed to swiftUI

---
+++ Scripty/Scripty/ScriptyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScriptyApp.swift
+//  Scripty
+//
+//  Created by Michael Testut on 2/1/23.
+//
+
+import SwiftUI
+
+@main
+struct ScriptyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
